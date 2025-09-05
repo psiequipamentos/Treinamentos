2c0b612057127deb43083647c55fd4504345c5e0
psiequipamentos <93715230+psiequipamentos@users.noreply.github.com>
2025-09-05 14:38:49 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,24 @@
-const app = require('../backend/src/index');
+// backend/src/index.js
+const express = require("express");
+const app = express();
 
+// Middlewares
+app.use(express.json());
+
+// Importa rotas
+const perfisRoutes = require("./routes/perfis");
+const contasRoutes = require("./routes/contas");
+const movimentacoesRoutes = require("./routes/movimentacoes");
+
+// Usa as rotas
+app.use("/perfis", perfisRoutes);
+app.use("/contas", contasRoutes);
+app.use("/movimentacoes", movimentacoesRoutes);
+
+// Rota default (teste)
+app.get("/", (req, res) => {
+  res.send("API rodando na Vercel 🚀");
+});
+
+// Exporta sem app.listen()
 module.exports = app;
